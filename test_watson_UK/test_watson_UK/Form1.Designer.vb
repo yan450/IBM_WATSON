76089d6b7069ef5b432ebc057f75a1b23f53759a
yan450 <yanfrenette@gmail.com>
2017-05-26 14:18:43 -0400

    Code

---
+++ test_watson_UK/test_watson_UK/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtResponse = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(260, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtResponse
+        '
+        Me.txtResponse.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtResponse.Location = New System.Drawing.Point(12, 42)
+        Me.txtResponse.Multiline = True
+        Me.txtResponse.Name = "txtResponse"
+        Me.txtResponse.Size = New System.Drawing.Size(260, 208)
+        Me.txtResponse.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.txtResponse)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtResponse As System.Windows.Forms.TextBox
+
+End Class
---
+++ test_watson_UK/test_watson_UK/Form1.vb
@@ -0,0 +1,67 @@
+﻿Imports System.Net
+Imports System.Text
+Imports System.IO
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        'Dim webClient As New System.Net.WebClient
+        'Dim result As String = webClient.DownloadString("https://y450.mybluemix.net/?SEND_AUDIO_FILE_LINK=blablabla")
+
+
+        ' Create a request using a URL that can receive a post.   
+        Dim request As WebRequest = WebRequest.Create("https://y450.mybluemix.net/SEND_AUDIO_FILE_LINK")
+
+        request.Method = "POST"
+        request.Timeout = 1000 * 60 * 10
+
+
+        ' Create POST data and convert it to a byte array.  
+        Dim postData As String = "{""url"": ""https://github.com/yan450/IBM_WATSON/blob/master/67110002355133_01.wav?raw=true""}"
+        Dim byteArray As Byte() = Encoding.UTF8.GetBytes(postData)
+        ' Set the ContentType property of the WebRequest.  
+        request.ContentType = "application/json"
+        ' Set the ContentLength property of the WebRequest.  
+        request.ContentLength = byteArray.Length
+        ' Get the request stream.  
+        Dim dataStream As Stream = request.GetRequestStream()
+        ' Write the data to the request stream.  
+        dataStream.Write(byteArray, 0, byteArray.Length)
+        ' Close the Stream object.  
+
+        txtResponse.Text = "SENT!"
+        Me.Refresh()
+
+        dataStream.Close()
+        'Get the response.  
+
+
+
+
+
+        Dim response As WebResponse = request.GetResponse()
+        
+        ' Get the stream containing content returned by the server.  
+        dataStream = response.GetResponseStream()
+        ' Open the stream using a StreamReader for easy access.  
+        Dim reader As New StreamReader(dataStream)
+        ' Read the content.  
+        Dim responseFromServer As String = reader.ReadToEnd()
+        ' Display the content.  
+        txtResponse.Text = responseFromServer
+        ' Clean up the streams.  
+        reader.Close()
+        dataStream.Close()
+        response.Close()
+
+
+    End Sub
+
+
+    Private Function sendRequest(ByVal codeID As String, ByVal totalAmount As String) As String
+
+
+
+    End Function
+
+End Class
